b71d9aa0320e9d0119948f5b09058e0f9a48e5a8
Jonathan <jonathan.tierno@gmail.com>
2016-05-31 17:12:04 -0300

    cambios en la fft para mostrarla mejor

--- main.m
+++ main.m
@@ -1,13 +1,24 @@
 close all,clear all;
+pkg load signal;
 FRECUENCIA_MUESTREO  = 4096;       % frec de muestreo
 ARCHIVO_MUESTRAS =  'L-L1_LOSC_4_V1-1126259446-32.txt';
 
 %CARGO LA SEÑAL DESDE EL ARCHIVO
 senial = load (ARCHIVO_MUESTRAS);
 senial = transpose(senial);
+N = length(senial);
+senial = senial - mean(senial);
 %DIBUJO LA SEÑAL EN FUNCION DEL TIEMPO
 timeline = printsignal(senial, FRECUENCIA_MUESTREO);
 
    
- printspect(senial, FRECUENCIA_MUESTREO);       
-          
+printspect(senial, FRECUENCIA_MUESTREO);       
+
+#w1 = hamming(400);  % ventana hamming
+#Y = fft(senial);
+#figure, specgram(senial, 1000) 
+#[b,a] = butter(1, [30/(2*pi * FRECUENCIA_MUESTREO), 400/(2*pi * FRECUENCIA_MUESTREO)]);
+#filtered = filter(b,a,senial);
+#printsignal(filtered, FRECUENCIA_MUESTREO);
+#printspect(filtered, FRECUENCIA_MUESTREO); 
+#plot(timeline, filtered);
--- printspect.m
+++ printspect.m
@@ -1,22 +1,13 @@
 function printspect(senial, samplingFrec)
-    N = length(senial);
+    N = length(senial); 
     spect = fft(senial); % hago la DFT
-    Y_eje = samplingFrec * [0:N-1]/N;
+    spect = fftshift(spect);
+    eje_frec = linspace(-samplingFrec/2, samplingFrec/2, N);
     figure
-    subplot (2, 1, 1)
-    plot(Y_eje,abs(spect));
+    plot(eje_frec,abs(spect));
     T= title ("Espectro vs Frecuencia");
     set (T, "fontsize", 12); 
     xlabel ("frecuencia [Hz]");
     ylabel ("espectro (módulo)");
-    #L = legend(strcat(["Intensidad \nDuracion ",num2str(duration)," seg"]));
-    #set (L, "fontsize", 10);   
-
-    spect(1) = 0; %filtro la continua;
-    subplot (2, 1, 2)
-    plot(Y_eje,abs(spect))   
-    T= title ("Espectro vs Frecuencia (sin continua)");
-    set (T, "fontsize", 12); 
-    xlabel ("frecuencia [Hz]");
-    ylabel ("espectro (módulo)");
+   
 endfunction
