730aee9ef481ed39ab7236d647679569a2ef9bb9
Jonathan <jonathan.tierno@gmail.com>
2016-06-06 22:52:03 -0300

    Listo, se ve la chirp a los 17 segundos

--- main2.m
+++ main2.m
@@ -5,7 +5,7 @@ FRECUENCIA_MUESTREO  = 4096;       % frec de muestreo
 ARCHIVO_MUESTRAS =  'L-L1_LOSC_4_V1-1126259446-32.txt';
 senial = load (ARCHIVO_MUESTRAS);
 N = length(senial);
-
+nyquist = FRECUENCIA_MUESTREO/2;
 
 %% Filtrado
 
@@ -13,11 +13,12 @@ N = length(senial);
 senial = senial - mean(senial);
 
 %paso un pasabanda 35-350
-b = fir1(5000,[35/FRECUENCIA_MUESTREO 350/FRECUENCIA_MUESTREO]);
-filtered = filter(b,1,senial);
+b = fir1(5000,[35/nyquist 350/nyquist]);
+filtrada1 = filter(b,1,senial);
 
+%filtrada2=filtrada1
 %la ventaneo
-recortada = filtered;
+recortada = filtrada1;
 for K = 1:10*FRECUENCIA_MUESTREO
     recortada(K) =0;
 end
@@ -25,23 +26,57 @@ for K = 22*FRECUENCIA_MUESTREO:length(senial)-1
     recortada(K)=0;
 end
 
+
+%saco unos picos que molestan (esto no mejora en nada
+filtrada2 = recortada;
+b = fir1(5000,[300/nyquist 360/nyquist],'stop');
+%filtrada2 = filter(b,1,filtrada2);
+b = fir1(5000,[10/nyquist 100/nyquist],'stop');
+%filtrada2 = filter(b,1,filtrada2);
+b = fir1(5000,[175/nyquist 200/nyquist],'stop');
+%filtrada2 = filter(b,1,filtrada2);
+b = fir1(5000,[330/nyquist 335/nyquist],'stop');
+%filtrada2 = filter(b,1,filtrada2);
+b = fir1(5000,[304/nyquist 319/nyquist],'stop');
+%filtrada2 = filter(b,1,filtrada2);
+b = fir1(5000,[58/nyquist 62/nyquist],'stop');
+%filtrada2 = filter(b,1,filtrada2);
+b = fir1(5000,[178/nyquist 182/nyquist],'stop');
+%filtrada2 = filter(b,1,filtrada2);
+
+
+
+
+
 %% Espectros
-subplot(3,1,1), printspect(senial, FRECUENCIA_MUESTREO); 
+
+
+close all;
+subplot(2,2,1), printspect(senial, FRECUENCIA_MUESTREO); 
 title('Espectro vs Frecuencia(original)');
 
-%dibujo el espectro de la filtrada
-subplot(3,1,2), printspect(filtered, FRECUENCIA_MUESTREO); 
-title('Espectro vs Frecuencia(filtrada)');
 
+%dibujo el espectro de la filtrada1
+subplot(2,2,2), printspect(filtrada1, FRECUENCIA_MUESTREO); 
+title('Espectro vs Frecuencia(pasabanda)');
 
 %dibujo el espectro de la recortada
-subplot(3,1,3), printspect(recortada, FRECUENCIA_MUESTREO); 
+subplot(2,2,3), printspect(recortada, FRECUENCIA_MUESTREO); 
 title('Espectro vs Frecuencia(ventaneada)');
 
-%% Espectrograma
+%dibujo el espectro de la filtrada2
+subplot(2,2,4), printspect(filtrada2, FRECUENCIA_MUESTREO); 
+title('Espectro vs Frecuencia(notchs)');
 
+
+
+
+%% Espectrograma
+close all;
 w = 256;
-ov = 128;
+ov = 200;
 nfft = FRECUENCIA_MUESTREO;
-figure, spectrogram(recortada/max(abs(recortada)),w, ov,[], nfft, 'yaxis')
+%figure, spectrogram(filtrada1/max(abs(filtrada1)),w, ov,[], nfft, 'yaxis')
+%no se ve bien la que no es ni recortada ni filtrada
+figure, spectrogram(recortada/max(abs(filtrada2)),w, ov,[], nfft, 'yaxis')
 axis ([15 20 0.03 0.35]);
